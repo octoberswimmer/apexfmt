2eee12317a2c4a0b60a89dcbfcef3f71e54f98dd
Christian G. Warden <cwarden@xerus.org>
2023-11-28 07:09:38 -0600

    Interface Declarations

--- main.go
+++ main.go
@@ -37,6 +37,7 @@ func (v *Visitor) VisitCompilationUnit(ctx *parser.CompilationUnitContext) inter
 		return v.visitRule(t.ClassDeclaration()).(string)
 	case t.InterfaceDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "IT'S AN INTERFACE")
+		return v.visitRule(t.InterfaceDeclaration()).(string)
 	case t.EnumDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "IT'S AN ENUM")
 	}
@@ -47,6 +48,21 @@ func (v *Visitor) VisitClassDeclaration(ctx *parser.ClassDeclarationContext) int
 	fmt.Fprintln(os.Stderr, "IN THE CLASS DECLARATION")
 	// TODO: handle extends and implements
 	return fmt.Sprintf("class %s {\n%s\n}", ctx.Id().GetText(), v.visitRule(ctx.ClassBody()))
+}
+
+func (v *Visitor) VisitInterfaceDeclaration(ctx *parser.InterfaceDeclarationContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN THE INTERFACE DECLARATION")
+	// TODO: handle extends
+	return fmt.Sprintf("interface %s {\n%s\n}", ctx.Id().GetText(), v.visitRule(ctx.InterfaceBody()))
+}
+
+func (v *Visitor) VisitInterfaceBody(ctx *parser.InterfaceBodyContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN THE INTERFACE BODY")
+	declarations := []string{}
+	for _, d := range ctx.AllInterfaceMethodDeclaration() {
+		declarations = append(declarations, v.visitRule(d).(string))
+	}
+	return strings.Join(declarations, "\n")
 }
 
 func (v *Visitor) VisitClassBody(ctx *parser.ClassBodyContext) interface{} {
@@ -105,7 +121,45 @@ func (v *Visitor) VisitMemberDeclaration(ctx *parser.MemberDeclarationContext) i
 	return ""
 }
 
+func (v *Visitor) VisitInterfaceMethodDeclaration(ctx *parser.InterfaceMethodDeclarationContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN INTERFACE METHOD DECLARATION")
+	returnType := "void"
+	if ctx.TypeRef() != nil {
+		returnType = v.visitRule(ctx.TypeRef()).(string)
+	}
+	mods := []string{}
+	for _, m := range ctx.AllModifier() {
+		mods = append(mods, m.GetText())
+	}
+	// TODO: formalParameters
+	return fmt.Sprintf("%s %s %s();", strings.Join(mods, " "), returnType, ctx.Id().GetText())
+}
+
+func (v *Visitor) VisitFieldDeclaration(ctx *parser.FieldDeclarationContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN FIELD DECLARATION")
+	return fmt.Sprintf("%s %s;", v.visitRule(ctx.TypeRef()), v.visitRule(ctx.VariableDeclarators()))
+}
+
+func (v *Visitor) VisitVariableDeclarators(ctx *parser.VariableDeclaratorsContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN VARIABLE DECLARATORS")
+	vars := []string{}
+	for _, vd := range ctx.AllVariableDeclarator() {
+		vars = append(vars, v.visitRule(vd).(string))
+	}
+	return strings.Join(vars, ", ")
+}
+
+func (v *Visitor) VisitVariableDeclarator(ctx *parser.VariableDeclaratorContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN VARIABLE DECLARATOR")
+	decl := ctx.Id().GetText()
+	if ctx.Expression() != nil {
+		decl = fmt.Sprintf("%s = %s", decl, v.visitRule(ctx.Expression()))
+	}
+	return decl
+}
+
 func (v *Visitor) VisitMethodDeclaration(ctx *parser.MethodDeclarationContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN METHOD DECLARATION")
 	returnType := "void"
 	if ctx.TypeRef() != nil {
 		returnType = v.visitRule(ctx.TypeRef()).(string)
