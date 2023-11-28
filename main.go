adfb8508410d8f5273da25bf8dbde22dffd6245d
Christian G. Warden <cwarden@xerus.org>
2023-11-28 13:50:43 -0600

    Ident Blocks; Handle More Rules

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -30,15 +31,15 @@ func (v *Visitor) visitRule(node antlr.RuleNode) interface{} {
 func (v *Visitor) VisitCompilationUnit(ctx *parser.CompilationUnitContext) interface{} {
 	fmt.Fprintln(os.Stderr, "HERE WE GO!")
 	t := ctx.TypeDeclaration()
-	// TODO: Handle the typeDeclaration modifiers
 	switch {
 	case t.ClassDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "IT'S A CLASS")
-		return v.visitRule(t.ClassDeclaration()).(string)
+		return fmt.Sprintf("%s%s", modifiers(t.AllModifier()), v.visitRule(t.ClassDeclaration()).(string))
 	case t.InterfaceDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "IT'S AN INTERFACE")
-		return v.visitRule(t.InterfaceDeclaration()).(string)
+		return fmt.Sprintf("%s%s", modifiers(t.AllModifier()), v.visitRule(t.InterfaceDeclaration()).(string))
 	case t.EnumDeclaration() != nil:
+		// TODO: Handle Enums
 		fmt.Fprintln(os.Stderr, "IT'S AN ENUM")
 	}
 	return ""
@@ -46,14 +47,38 @@ func (v *Visitor) VisitCompilationUnit(ctx *parser.CompilationUnitContext) inter
 
 func (v *Visitor) VisitClassDeclaration(ctx *parser.ClassDeclarationContext) interface{} {
 	fmt.Fprintln(os.Stderr, "IN THE CLASS DECLARATION")
-	// TODO: handle extends and implements
-	return fmt.Sprintf("class %s {\n%s\n}", ctx.Id().GetText(), v.visitRule(ctx.ClassBody()))
+	extends := ""
+	if ctx.EXTENDS() != nil {
+		extends = fmt.Sprintf(" extends %s ", v.VisitTypeRef(ctx.TypeRef().(*parser.TypeRefContext)))
+	}
+	implements := ""
+	if ctx.IMPLEMENTS() != nil {
+		extends = fmt.Sprintf(" implements %s ", v.VisitTypeList(ctx.TypeList().(*parser.TypeListContext)))
+	}
+	return fmt.Sprintf("class %s%s%s{\n%s\n}", ctx.Id().GetText(),
+		extends,
+		implements,
+		indent(v.visitRule(ctx.ClassBody()).(string)))
+}
+
+func indent(text string) string {
+	var indentedText strings.Builder
+	scanner := bufio.NewScanner(strings.NewReader(text))
+
+	for scanner.Scan() {
+		indentedText.WriteString("\t" + scanner.Text() + "\n")
+	}
+
+	return indentedText.String()
 }
 
 func (v *Visitor) VisitInterfaceDeclaration(ctx *parser.InterfaceDeclarationContext) interface{} {
 	fmt.Fprintln(os.Stderr, "IN THE INTERFACE DECLARATION")
-	// TODO: handle extends
-	return fmt.Sprintf("interface %s {\n%s\n}", ctx.Id().GetText(), v.visitRule(ctx.InterfaceBody()))
+	extends := ""
+	if ctx.EXTENDS() != nil {
+		extends = fmt.Sprintf(" extends %s ", v.VisitTypeList(ctx.TypeList().(*parser.TypeListContext)))
+	}
+	return fmt.Sprintf("interface %s%s {\n%s\n}", ctx.Id().GetText(), extends, indent(v.visitRule(ctx.InterfaceBody()).(string)))
 }
 
 func (v *Visitor) VisitInterfaceBody(ctx *parser.InterfaceBodyContext) interface{} {
@@ -80,30 +105,33 @@ func (v *Visitor) VisitClassBodyDeclaration(ctx *parser.ClassBodyDeclarationCont
 		return ";"
 	case ctx.Block() != nil:
 		// TODO: Handle static
-		fmt.Fprintln(os.Stderr, "GOT A BLOCK")
-	case ctx.MemberDeclaration() != nil:
-		fmt.Fprintln(os.Stderr, "IT'S A MEMBER")
-		mods := []string{}
-		for _, m := range ctx.AllModifier() {
-			mods = append(mods, m.GetText())
+		static := ""
+		if ctx.STATIC() != nil {
+			static = "static "
 		}
-		return fmt.Sprintf("%s %s", strings.Join(mods, " "), v.visitRule(ctx.MemberDeclaration()))
+		fmt.Fprintln(os.Stderr, "GOT A BLOCK")
+		return fmt.Sprintf("%s%s", static, indent(v.VisitBlock(ctx.Block().(*parser.BlockContext)).(string)))
+	case ctx.MemberDeclaration() != nil:
+		// fmt.Fprintln(os.Stderr, "IT'S A MEMBER")
+		return fmt.Sprintf("%s%s", modifiers(ctx.AllModifier()), v.visitRule(ctx.MemberDeclaration()))
 	}
 	return ""
 }
 
 func (v *Visitor) VisitMemberDeclaration(ctx *parser.MemberDeclarationContext) interface{} {
-	fmt.Fprintln(os.Stderr, "IN MEMBER DECLARATION")
+	// fmt.Fprintln(os.Stderr, "IN MEMBER DECLARATION")
 	switch {
 	case ctx.MethodDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "FOUND METHOD DECLARATION")
 		return v.visitRule(ctx.MethodDeclaration())
 	case ctx.FieldDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "FOUND FIELD DECLARATION")
-		return v.visitRule(ctx.FieldDeclaration())
+		// return v.visitRule(ctx.FieldDeclaration())
+		return v.VisitFieldDeclaration(ctx.FieldDeclaration().(*parser.FieldDeclarationContext))
 	case ctx.ConstructorDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "FOUND CONSTRUCTOR DECLARATION")
-		return v.visitRule(ctx.ConstructorDeclaration())
+		// return v.visitRule(ctx.ConstructorDeclaration())
+		return v.VisitConstructorDeclaration(ctx.ConstructorDeclaration().(*parser.ConstructorDeclarationContext))
 	case ctx.InterfaceDeclaration() != nil:
 		fmt.Fprintln(os.Stderr, "FOUND INTERFACE DECLARATION")
 		return v.visitRule(ctx.InterfaceDeclaration())
@@ -127,17 +155,70 @@ func (v *Visitor) VisitInterfaceMethodDeclaration(ctx *parser.InterfaceMethodDec
 	if ctx.TypeRef() != nil {
 		returnType = v.visitRule(ctx.TypeRef()).(string)
 	}
-	mods := []string{}
-	for _, m := range ctx.AllModifier() {
-		mods = append(mods, m.GetText())
-	}
-	// TODO: formalParameters
-	return fmt.Sprintf("%s %s %s();", strings.Join(mods, " "), returnType, ctx.Id().GetText())
+	return fmt.Sprintf("%s%s %s%s;", modifiers(ctx.AllModifier()), returnType, ctx.Id().GetText(), v.visitRule(ctx.FormalParameters()))
 }
 
 func (v *Visitor) VisitFieldDeclaration(ctx *parser.FieldDeclarationContext) interface{} {
 	fmt.Fprintln(os.Stderr, "IN FIELD DECLARATION")
 	return fmt.Sprintf("%s %s;", v.visitRule(ctx.TypeRef()), v.visitRule(ctx.VariableDeclarators()))
+}
+
+func (v *Visitor) VisitConstructorDeclaration(ctx *parser.ConstructorDeclarationContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN CONSTRUCTOR DECLARATION")
+	return fmt.Sprintf("%s%s {\n%s}", v.visitRule(ctx.QualifiedName()), v.visitRule(ctx.FormalParameters()), indent(v.visitRule(ctx.Block()).(string)))
+}
+
+func (v *Visitor) VisitBlock(ctx *parser.BlockContext) interface{} {
+	return "BLOCK"
+}
+
+func (v *Visitor) VisitTypeList(ctx *parser.TypeListContext) interface{} {
+	types := []string{}
+	for _, p := range ctx.AllTypeRef() {
+		types = append(types, v.visitRule(p).(string))
+	}
+	return strings.Join(types, ", ")
+}
+
+func (v *Visitor) VisitFormalParameters(ctx *parser.FormalParametersContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN FORMAL PARAMETERS")
+	params := []string{}
+	list := ctx.FormalParameterList()
+	if list == nil {
+		return "()"
+	}
+	for _, p := range list.AllFormalParameter() {
+		params = append(params, v.visitRule(p).(string))
+	}
+	val := fmt.Sprintf("(%s)", strings.Join(params, ", "))
+	fmt.Fprintf(os.Stderr, "FORMAL PARAMETERS:|%s|\n", val)
+	return val
+}
+
+func modifiers(ctxs []parser.IModifierContext) string {
+	mods := []string{}
+	for _, m := range ctxs {
+		mods = append(mods, m.GetText())
+	}
+	modifiers := strings.Join(mods, " ")
+	if modifiers != "" {
+		modifiers += " "
+	}
+	return modifiers
+}
+
+func (v *Visitor) VisitFormalParameter(ctx *parser.FormalParameterContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN FORMAL PARAMETER")
+	return fmt.Sprintf("%s%s %s", modifiers(ctx.AllModifier()), v.visitRule(ctx.TypeRef()), ctx.Id().GetText())
+}
+
+func (v *Visitor) VisitQualifiedName(ctx *parser.QualifiedNameContext) interface{} {
+	fmt.Fprintln(os.Stderr, "IN QUALIFIED NAME")
+	ids := []string{}
+	for _, i := range ctx.AllId() {
+		ids = append(ids, i.GetText())
+	}
+	return strings.Join(ids, ".")
 }
 
 func (v *Visitor) VisitVariableDeclarators(ctx *parser.VariableDeclaratorsContext) interface{} {
@@ -164,8 +245,9 @@ func (v *Visitor) VisitMethodDeclaration(ctx *parser.MethodDeclarationContext) i
 	if ctx.TypeRef() != nil {
 		returnType = v.visitRule(ctx.TypeRef()).(string)
 	}
-	// TODO: formalParameters
-	return fmt.Sprintf("%s %s() {\n%s\n}\n", returnType, ctx.Id().GetText(), "")
+	return fmt.Sprintf("%s %s%s {\n%s}\n", returnType, ctx.Id().GetText(),
+		v.VisitFormalParameters(ctx.FormalParameters().(*parser.FormalParametersContext)),
+		indent(v.VisitBlock(ctx.Block().(*parser.BlockContext)).(string)))
 }
 
 func (v *Visitor) VisitTypeRef(ctx *parser.TypeRefContext) interface{} {
@@ -175,7 +257,10 @@ func (v *Visitor) VisitTypeRef(ctx *parser.TypeRefContext) interface{} {
 		typeNames = append(typeNames, t.GetText())
 	}
 
-	return fmt.Sprintf("%s%s", strings.Join(typeNames, "."), ctx.ArraySubscripts().GetText())
+	fmt.Fprintf(os.Stderr, "TYPE NAMES:|%s|\n", strings.Join(typeNames, "."))
+
+	val := fmt.Sprintf("%s%s", strings.Join(typeNames, "."), ctx.ArraySubscripts().GetText())
+	return val
 }
 
 func main() {
