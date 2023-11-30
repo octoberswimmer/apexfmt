c8917aea88e71e05055dcbcf4e8a1ef388cb8134
Christian G. Warden <cwarden@xerus.org>
2023-11-30 09:55:03 -0600

    Compare Original File to Formatted File

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/antlr4-go/antlr/v4"
@@ -10,7 +11,12 @@ import (
 )
 
 func main() {
-	input, _ := antlr.NewFileStream(os.Args[1])
+	src, err := readFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read file: %s\n", err.Error())
+		os.Exit(1)
+	}
+	input := antlr.NewInputStream(string(src))
 	lexer := parser.NewApexLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 
@@ -22,4 +28,21 @@ func main() {
 	if !ok {
 	}
 	fmt.Println(out)
+	if string(src) != out {
+		fmt.Fprintf(os.Stderr, "Content changed\n")
+		os.Exit(1)
+	}
+}
+
+func readFile(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	src, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
 }
